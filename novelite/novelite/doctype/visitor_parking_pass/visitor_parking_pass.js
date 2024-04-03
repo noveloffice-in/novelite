c4cca8b29b31122cbf2fc63628fb78e1a1271177
crazy-explore-r <developer.ragul@gmail.com>
2024-04-03 15:11:36 +0530

    chore: doctype create

---
+++ novelite/novelite/doctype/visitor_parking_pass/visitor_parking_pass.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ragul KM and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Visitor Parking Pass', {
+	// refresh: function(frm) {
+
+	// }
+});
