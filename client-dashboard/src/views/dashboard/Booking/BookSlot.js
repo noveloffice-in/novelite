b06ce0d00b1d0115bc3d0c1ad87e1b6d3d595dc5
crazy-explore-r <developer.ragul@gmail.com>
2024-04-24 14:55:57 +0530

    chore: new comp for booking

--- client-dashboard/src/views/dashboard/Booking/BookSlot.js
+++ client-dashboard/src/views/dashboard/Booking/BookSlot.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageContainer from '../../../components/container/PageContainer';
 import Breadcrumb from '../../../layouts/full/shared/breadcrumb/Breadcrumb';
 import { useSelector } from 'react-redux';
@@ -13,7 +13,9 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 //Time
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import dayjs from 'dayjs';
-import { TextField } from '@mui/material';
+import { Button, CircularProgress, TextField } from '@mui/material';
+import ChildCard from '../../../components/shared/ChildCard';
+import { useFrappeCreateDoc } from 'frappe-react-sdk';
 
 
 export default function BookSlot() {
@@ -25,11 +27,6 @@ export default function BookSlot() {
     const companyName = useSelector((state) => state.novelprofileReducer.companyName);
     const confirmed_location = useSelector((state) => state.novelprofileReducer.location);
     const leadsID = useSelector((state) => state.novelprofileReducer.leadsID);
-
-    const [value, setValue] = React.useState(() => [
-        dayjs('2022-04-17T15:30'),
-        dayjs('2022-04-17T18:30'),
-    ]);
 
     const BCrumb = [
         {
@@ -54,49 +51,109 @@ export default function BookSlot() {
         },
     ];
 
+    const [date, setDate] = useState(dayjs());
+    const [fromTime, setFromTime] = useState(dayjs());
+    const [toTime, setToTime] = useState(dayjs());
+
+
+    //Date change
+    const handleDateChange = (newValue) => {
+        const date = new Date(newValue);
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        setDate(formattedDate);
+    };
+
+    //Time Change
+    const handleTimeChange = (newValue, change) => {
+        const date = new Date(newValue);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        const formattedHours = hours % 12 || 12;
+        const formattedTime = `${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+        if (change === 'from') {
+            setFromTime(formattedTime);
+        } else {
+            setToTime(formattedTime);
+        }
+    };
+
+    //Submit
+    const { createDoc, loading } = useFrappeCreateDoc();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let form = new FormData(e.target);
+        let formObj = Object.fromEntries(form.entries());
+
+        const boookingData = {
+            customer: companyName,
+            location: location,
+            room_type: roomType,
+            room: roomName,
+            booking_date: date,
+            from_time: fromTime,
+            to_time: toTime,
+            description: formObj.description
+        }
+
+        createDoc('Room slots booking', boookingData)
+            .then(() => {
+                console.log('Data Added');
+            }).catch((err) => {
+                console.log("inside catch " + JSON.stringify(err.message));
+                console.err(err.message);
+            })
+
+        console.log(formObj);
+        console.log("Date = ", date);
+        console.log("From time = ", fromTime);
+        console.log("toTime = ", toTime);
+    }
+
     return (
         <PageContainer title="Book Slots - Novel Office">
             <Breadcrumb title="Booking Slots" items={BCrumb} />
-            <Stack justifyContent='center' alignItems='center' width='100%'>
-                {/* <Box width='500px'> */}
-                    <Stack width='100%' justifyContent='center' alignItems='center'>
+            <ChildCard justifyContent='center' alignItems='center' width='100%'>
+                <Stack width='100%' justifyContent='center' alignItems='center'>
 
+                    <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: '1rem' }}>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <DemoContainer components={['DatePicker']}>
-                                <DatePicker label="Plesase select date" />
+                                <DatePicker label="Plesase select date" value={date} onChange={(newValue) => { handleDateChange(newValue.$d) }} />
                             </DemoContainer>
                         </LocalizationProvider>
 
 
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <DemoContainer components={['TimePicker']}>
-                                <TimePicker label="Basic time picker" />
+                                <TimePicker label="From" value={fromTime} onChange={(newValue) => { handleTimeChange(newValue, 'from') }} />
                             </DemoContainer>
                         </LocalizationProvider>
 
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <DemoContainer components={['TimePicker']}>
-                                <TimePicker label="Basic time picker" />
+                                <TimePicker label="To" value={toTime} onChange={(newValue) => { handleTimeChange(newValue, 'to') }} />
                             </DemoContainer>
                         </LocalizationProvider>
 
-
-
-                        <Box sx={{ mt: 2 }} >
+                        <Box >
                             <TextField
                                 id="outlined-multiline-static"
-                                label="Ticket Description"
+                                label="Description"
                                 multiline
                                 rows={2}
-                                style={{ maxWidth: '100%' }}
+                                style={{ width: '14.5rem' }}
                                 name="description"
-                            // value={ticketData.description}
-                            // onChange={handleTicketDataChange}
                             />
                         </Box>
-                    </Stack>
-                {/* </Box> */}
-            </Stack>
+
+                        <Button type='submit' variant="contained">
+                            {loading ? <CircularProgress color="inherit" size={26} /> : "Submit"}
+                        </Button>
+                    </form>
+
+                </Stack>
+            </ChildCard>
         </PageContainer>
     )
 }
