67b0fabb43b835d7f53f5a7fd4172555788b678b
deviprasadkl <deviprasadofficial@gmail.com>
2024-06-13 17:50:47 +0530

    del: app members

--- novelite/novelite/doctype/app_members/app_members.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Ragul KM and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class AppMembers(Document):
-	pass
